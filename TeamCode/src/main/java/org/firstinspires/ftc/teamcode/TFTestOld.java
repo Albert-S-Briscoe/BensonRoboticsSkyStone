ff30f07ddb1610f258a6281c87f0081f8727348b
gavin4d <gavin4d05@gmail.com>
2020-03-02 19:56:53 -0800

    new files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TFTestOld.java
@@ -0,0 +1,213 @@
+/* Copyright (c) 2019 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
+
+/**
+ * This 2019-2020 OpMode illustrates the basics of using the Vuforia localizer to determine
+ * positioning and orientation of robot on the SKYSTONE FTC field.
+ * The code is structured as a LinearOpMode
+ *
+ * When images are located, Vuforia is able to determine the position and orientation of the
+ * image relative to the camera.  This sample code then combines that information with a
+ * knowledge of where the target images are on the field, to determine the location of the camera.
+ *
+ * From the Audience perspective, the Red Alliance station is on the right and the
+ * Blue Alliance Station is on the left.
+
+ * Eight perimeter targets are distributed evenly around the four perimeter walls
+ * Four Bridge targets are located on the bridge uprights.
+ * Refer to the Field Setup manual for more specific location details
+ *
+ * A final calculation then uses the location of the camera on the robot to determine the
+ * robot's location and orientation on the field.
+ *
+ * @see VuforiaLocalizer
+ * @see VuforiaTrackableDefaultListener
+ * see  skystone/doc/tutorial/FTC_FieldCoordinateSystemDefinition.pdf
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
+ *
+ * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
+ * is explained below.
+ */
+
+
+@TeleOp(name="TF Old", group ="Concept")
+//@Disabled
+public class TFTestOld extends LinearOpMode {
+
+    // IMPORTANT:  For Phone Camera, setMoveInches 1) the camera source and 2) the orientation, based on how your phone is mounted:
+    // 1) Camera Source.  Valid choices are:  BACK (behind screen) or FRONT (selfie side)
+    // 2) Phone Orientation. Choices are: PHONE_IS_PORTRAIT = true (portrait) or PHONE_IS_PORTRAIT = false (landscape)
+    //
+    // NOTE: If you are running on a CONTROL HUB, with only one USB WebCam, you must select CAMERA_CHOICE = BACK; and PHONE_IS_PORTRAIT = false;
+    //
+    private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
+    private static final boolean PHONE_IS_PORTRAIT = false;
+
+    /*
+     * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
+     * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
+     * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
+     * web site at https://developer.vuforia.com/license-manager.
+     *
+     * Vuforia license keys are always 380 characters long, and look as if they contain mostly
+     * random data. As an example, here is a example of a fragment of a valid key:
+     *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
+     * Once you've obtained a license key, copy the string from the Vuforia web site
+     * and paste it in to your code on the next line, between the double quotes.
+     */
+    private static final String VUFORIA_KEY = "AXfJetz/////AAABmfTftTQRKUq2u+iCzbuFm2wKhp5/qubTF+6xF9VBwMBiVi2lCwJbNrIAVofnUKke4/MjFtZROHGeelAgbQx6MjYX+qdX4vRB5z2PboepftoqvoZy3irQKQ2aKqNSbpN72hI/tI2wluN0xqC6KThtMURH0EuvUf8VcGDfmuXiA/uP00/2dsYhIMhxBJCmBq0AG5jMWi8MnHJDZwnoYLdcliKB7rvNTUDbf1fzxRzf9QHgB2u+invzPou7q8ncAsD5GdXFfA/CiYmR65JKXDOE0wHoc8FxvrzUIRCQ2geSypo7eY5q/STJvqPmjoj33CQFHl0hKMx05QwwsABdlIZvfLLbjA3VH2HO4dcv+OOoElws";
+
+
+    // Since ImageTarget trackables use mm to specifiy their dimensions, we must use mm for all the physical dimension.
+    // We will define some constants and conversions here
+    private static final float mmPerInch        = 25.4f;
+    private static final float mmTargetHeight   = (6) * mmPerInch;          // the height of the center of the target image above the floor
+
+    // Constant for Stone Target
+    private static final float stoneZ = 2.00f * mmPerInch;
+
+    // Constants for the center support targets
+    private static final float bridgeZ = 6.42f * mmPerInch;
+    private static final float bridgeY = 23 * mmPerInch;
+    private static final float bridgeX = 5.18f * mmPerInch;
+    private static final float bridgeRotY = 59;                                 // Units are degrees
+    private static final float bridgeRotZ = 180;
+
+    // Constants for perimeter targets
+    private static final float halfField = 72 * mmPerInch;
+    private static final float quadField  = 36 * mmPerInch;
+
+    // Class Members
+    private OpenGLMatrix lastLocation = null;
+    private VuforiaLocalizer vuforia = null;
+    private boolean targetVisible = false;
+    private float phoneXRotate    = 0;
+    private float phoneYRotate    = 0;
+    private float phoneZRotate    = 0;
+
+    private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "SkyStone";
+    private static final String LABEL_SECOND_ELEMENT = "SkyStone";
+
+    private TFObjectDetector tfod;
+
+
+    @Override public void runOpMode() {
+        /*
+         * Configure Vuforia by creating a Parameter object, and passing it to the Vuforia engine.
+         * We can pass Vuforia the handle to a camera preview resource (on the RC phone);
+         * If no camera monitor is desired, use the parameter-less constructor instead (commented out below).
+         */
+
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        // VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = CAMERA_CHOICE;
+
+        //  Instantiate the Vuforia engine
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        initTfod();
+
+        if (tfod != null) {
+            tfod.activate();
+        }
+
+        // Load the data sets for the trackable objects. These particular data
+        // sets are stored in the 'assets' part of our application.
+
+
+        // WARNING:
+        // In this sample, we do not wait for PLAY to be pressed.  Target Tracking is started immediately when INIT is pressed.
+        // This sequence is used to enable the new remote DS Camera Preview feature to be used with this sample.
+        // CONSEQUENTLY do not put any driving commands in this loop.
+        // To restore the normal opmode structure, just un-comment the following line:
+
+        waitForStart();
+
+        // Note: To use the remote camera preview:
+        // AFTER you hit Init on the Driver Station, use the "options menu" to select "Camera Stream"
+        // Tap the preview window to receive a fresh image.
+
+        while (!isStopRequested()) {
+
+            if (tfod != null) {
+                // getUpdatedRecognitions() will return null if no new information is available since
+                // the last time that call was made.
+                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                if (updatedRecognitions != null) {
+                    telemetry.addData("# Object Detected", updatedRecognitions.size());
+
+                    // step through the list of recognitions and display boundary info.
+                    int i = 0;
+                    for (Recognition recognition : updatedRecognitions) {
+                        telemetry.addData(String.format("label (%d)", i), recognition.getLabel());
+                        telemetry.addData(String.format("  left,top (%d)", i), "%.03f , %.03f",
+                                recognition.getLeft(), recognition.getTop());
+                        telemetry.addData(String.format("  right,bottom (%d)", i), "%.03f , %.03f",
+                                recognition.getRight(), recognition.getBottom());
+                    }
+                }
+            }
+
+            // check all the trackable targets to see which one (if any) is visible.
+
+        }
+    }
+
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minimumConfidence = 0.8;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomousRed.java
@@ -0,0 +1,400 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection.BACK;
+
+@Autonomous(name="Autonomous Red", group="Linear Opmode")
+public class autonomousRed extends LinearOpMode {
+
+    private static final String VUFORIA_KEY = "AXfJetz/////AAABmfTftTQRKUq2u+iCzbuFm2wKhp5/qubTF+6xF9VBwMBiVi2lCwJbNrIAVofnUKke4/MjFtZROHGeelAgbQx6MjYX+qdX4vRB5z2PboepftoqvoZy3irQKQ2aKqNSbpN72hI/tI2wluN0xqC6KThtMURH0EuvUf8VcGDfmuXiA/uP00/2dsYhIMhxBJCmBq0AG5jMWi8MnHJDZwnoYLdcliKB7rvNTUDbf1fzxRzf9QHgB2u+invzPou7q8ncAsD5GdXFfA/CiYmR65JKXDOE0wHoc8FxvrzUIRCQ2geSypo7eY5q/STJvqPmjoj33CQFHl0hKMx05QwwsABdlIZvfLLbjA3VH2HO4dcv+OOoElws";
+    private static final String TFOD_MODEL_ASSET = "Skystone.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Skystone";
+
+    private VuforiaLocalizer vuforia;
+    private TFObjectDetector tfod;
+
+    private static final VuforiaLocalizer.CameraDirection CAMERA_CHOICE = BACK;
+
+    private ElapsedTime         runtime  = new ElapsedTime();
+    private RobotHardware       H        = new RobotHardware();
+    private MecanumWheelDriver  drive    = new MecanumWheelDriver(H);
+    private LinearArmDriver     arm      = new LinearArmDriver(H);
+    private ExecutorService     pool     = Executors.newFixedThreadPool(2);
+
+    private int mode = 1;
+
+    private int objleft;
+    private int objright;
+    private int objcenter;
+    private int offset;
+    private int blockPos;
+
+    //final double speed_slow = .35;
+    final double sidewaysInches = 1.4;
+    final double speed_norm = .6;
+    final double speed_fast = 1;
+    final byte field_side = -1;   // -1 = red, 1 = blue
+    final int maxOffsetForMiddelBlock = 120;
+    final double reasonability = 2;
+    
+    
+    private boolean found = false;
+    private double inches_to_move;
+
+    @Override
+    public void runOpMode() {
+
+        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
+
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        vuforia = ClassFactory.getInstance().createVuforia(parameters);
+
+        initTfod();
+
+        if (tfod != null) {
+            tfod.activate();
+        }
+
+        //drive.init(hardwareMap);
+        //arm.init(hardwareMap);
+        H.init(hardwareMap);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+        arm.zero();
+        pool.execute(arm);
+
+        H.grab(false);
+        arm.moveToInch(3);
+        H.block(false);
+        drive.RunWithEncoders(true);
+    
+        
+        if (opModeIsActive()) {
+            telemetry.addData("Status", "loop started");
+            telemetry.update();
+            findSkystone();
+        }
+        if (opModeIsActive()) {
+            _1A();
+        }
+        if (opModeIsActive()) {
+            pickUpSkystone();
+        }
+        if (opModeIsActive()) {
+            _2A();
+        }
+        if (opModeIsActive()) {
+            placeSkystoneAndGrab();
+        }
+        if (opModeIsActive()) {
+            _3A();
+        }
+        if (opModeIsActive()) {
+            _4A();
+        }
+
+        arm.stop();
+        drive.stop();
+        pool.shutdownNow();
+        H.leftfront.setPower(0);
+        H.rightfront.setPower(0);
+        H.leftback.setPower(0);
+        H.rightback.setPower(0);
+    }
+
+
+
+    private void _1A() {
+
+        arm.moveToInch(2.5);
+        
+        drive.setMoveInches(0,28, speed_fast, 0);
+        pool.execute(drive);
+        waitForMoveDone(0);
+        
+        /*while (!isStopRequested() && runtime.seconds() < 7 && !drive.moveDone && Math.abs(H.upperRange.getDistance(DistanceUnit.INCH) - (drive.H.leftback.getTargetPosition() - drive.H.leftback.getCurrentPosition()) / drive.COUNTS_PER_INCH) < reasonability) {
+            
+            drive.changeTargetInches(H.upperRange.getDistance(DistanceUnit.INCH) - 1, false);
+            
+        }
+
+        /*while (!isStopRequested() && H.upperRange.getDistance(DistanceUnit.MM) > 650) {
+            drive.move(0, speed_norm, 0);
+            if (runtime.seconds() > 4.5) {
+                drive.stop();
+                while (!isStopRequested()) {
+                    idle();
+                }
+            }
+        }*/
+
+    }
+
+    private void _1B() {
+
+    }
+
+    private void _2A() {
+
+        drive.setrotate(90 * field_side, speed_fast, true);
+        pool.execute(drive);
+        waitForMoveDone(0);
+
+        arm.moveToInch(0);
+
+        drive.setMoveInches(0, 84 + 8 * blockPos * field_side, speed_fast, 90 * field_side);
+        pool.execute(drive);
+        waitForMoveDone(0);
+
+    }
+
+    private void _2B() {
+
+    }
+
+    private void _3A() {
+        drive.move(-90 * field_side, speed_fast, 0);
+        sleep(500);
+        drive.stop();
+        drive.setMoveInches(180, 18, speed_fast, 0);
+        pool.execute(drive);
+        waitForMoveDone(0);
+        drive.setRampDown(25, 0.4);
+        drive.setrotate(90 * field_side, speed_fast, true);
+        pool.execute(drive);
+        waitForMoveDone(0);
+        drive.setMoveInches(0, 11, speed_fast, 90 * field_side);
+        pool.execute(drive);
+        drive.setRampDown(0, 0);
+        sleep(1000);
+        drive.stop();
+        waitForMoveDone(0);
+
+        H.grab(false);
+    }
+
+    private void _4A() {
+        drive.move(180, speed_fast, 0);
+        sleep(150);
+        drive.stop();
+        drive.move(-90 * field_side, speed_fast, 0);
+        sleep(300);
+        drive.stop();
+        drive.setrotate(90 * field_side, speed_fast, true);
+        pool.execute(drive);
+        waitForMoveDone(0);
+        drive.setMoveInches(180, 40, speed_fast, -1);
+        pool.execute(drive);
+        sleep(500);
+        arm.moveToInch(0);
+        waitForMoveDone(2);
+    }
+
+    private void _4B() {
+
+    }
+
+    private void _5A() {
+
+    }
+
+    private void _5B() {
+
+    }
+
+    private void _6A() {
+
+    }
+
+    private void _6B() {
+
+    }
+
+    private void _7A() {
+
+    }
+
+    private void _7B() {
+
+    }
+
+    private void findSkystone() {
+        while (!isStopRequested() && runtime.seconds() < 4 && !found) {
+            if (tfod != null) {
+                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                if (updatedRecognitions != null) {
+                    for (Recognition recognition : updatedRecognitions) {
+                        if (recognition.getLabel().equals("Skystone")) {
+                            objleft = (int) recognition.getLeft();
+                            objright = (int) recognition.getRight();
+                            objcenter = (objleft + objright) / 2;
+                            offset = objcenter - 320;
+                            if (objright - objleft < 250) {
+                                if (Math.abs(offset) < maxOffsetForMiddelBlock) {
+                                    blockPos = 0;
+                                } else {
+                                    blockPos = (int)Math.signum(offset);
+                                }
+                                found = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void pickUpSkystone() {
+        
+        if (!isStopRequested() && blockPos != 0) {
+            drive.setMoveInches(-90 * blockPos, 8.5 * sidewaysInches, speed_fast, 0);
+            pool.execute(drive);
+            sleep(1000);
+        }
+        drive.stop();
+    
+        arm.moveToInch(0);
+        
+        drive.setrotate(0, speed_fast, true);
+        pool.execute(drive);
+        waitForMoveDone(0);
+        sleep(500);
+        
+        
+        drive.setMoveInches(0, 2, speed_fast, 0);
+        pool.execute(drive);
+        waitForMoveDone(0);
+    
+        H.block(true);
+        
+        sleep(500);
+        
+        arm.moveToInch(6);
+        
+        drive.setMoveInches(180, 5, speed_fast, 0);
+        pool.execute(drive);
+        waitForMoveDone(0);
+    
+    }
+
+    private void placeSkystoneAndGrab() {
+        
+        arm.moveToInch(4);
+        sleep(400);
+        drive.setrotate(0, speed_fast, true);
+        pool.execute(drive);
+        waitForMoveDone(0);
+        if (H.lowerRange.getDistance(DistanceUnit.INCH) < 15) {
+            inches_to_move = H.lowerRange.getDistance(DistanceUnit.INCH) + 0.5;
+        } else {
+            inches_to_move = 6.5;
+        }
+        drive.setMoveInches(0, inches_to_move, speed_fast, 0);
+        pool.execute(drive);
+        arm.moveToInch(3);
+        waitForMoveDone(0);
+        H.block(false);
+        H.grab(true);
+        sleep(500);
+
+    }
+
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minimumConfidence = 0.75;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT);//, LABEL_SECOND_ELEMENT);
+    }
+
+    private void waitForMoveDone(int moveDriver) {
+
+        // 0 = MecanumWheelDriver
+        // 1 = ArmDriver
+        // 2 = both
+    
+        sleep(100);
+
+        switch (moveDriver) {
+            case 0:
+
+                while (!isStopRequested() && !drive.moveDone) {
+
+                    idle();
+
+                }
+                break;
+
+            case 1:
+
+                while (!isStopRequested() && !arm.moveDone) {
+
+                    idle();
+
+                }
+                break;
+
+            case 2:
+
+                while (!isStopRequested() && (!drive.moveDone || !arm.moveDone)) {
+
+                    idle();
+
+                }
+                break;
+
+        }
+
+    }
+
+}
